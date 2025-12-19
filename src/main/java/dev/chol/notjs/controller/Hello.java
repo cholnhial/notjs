42bbea0bb11750c4e0dee15f9989d8d55bb9dc51
Chol Nhial Chol <chol.nhial.chol@gmail.com>
2025-12-19 15:14:24 +1000

    the basics

---
+++ src/main/java/dev/chol/notjs/controller/Hello.java
@@ -0,0 +1,14 @@
+package dev.chol.notjs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+}
