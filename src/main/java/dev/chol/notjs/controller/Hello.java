8973fb73670112002c3e2b1aca1d364c392960d7
Chol Nhial Chol <chol.nhial.chol@gmail.com>
2025-12-20 13:59:03 +1000

    remove node folder

--- src/main/java/dev/chol/notjs/controller/Hello.java
+++ src/main/java/dev/chol/notjs/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @RequestMapping("/")
+    @RequestMapping("/asd")
     public String home() {
         return "Hello Docker World";
     }
