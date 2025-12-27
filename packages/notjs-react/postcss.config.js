d934084f3c4a5e469828e092e75c1febeca3c47c
Chol Nhial Chol <chol.nhial.chol@gmail.com>
2025-12-27 20:24:10 +1000

    bump up to version 0.3.1

--- packages/notjs-react/postcss.config.js
+++ packages/notjs-react/postcss.config.js
@@ -1,8 +1,10 @@
+import postcssImport from 'postcss-import';
 import tailwindcss from '@tailwindcss/postcss';
 import unwrapLayersPlugin from './unwrap-layers-plugin.js';
 
 export default {
   plugins: [
+    postcssImport(),
     tailwindcss(),
     unwrapLayersPlugin(),
   ],
