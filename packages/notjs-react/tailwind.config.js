d9f851ca8274f811c87d74c60be6d7912d8d67ad
Chol Nhial Chol <chol.nhial.chol@gmail.com>
2025-12-28 14:48:53 +1000

    release 0.5.4

--- packages/notjs-react/tailwind.config.js
+++ packages/notjs-react/tailwind.config.js
@@ -1,6 +1,7 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  darkMode: ['class', '.notjs-container'],
+  important: '.notjs-container', // Scope all utilities to .notjs-container
   corePlugins: {
     preflight: false, // Disable Tailwind's base reset styles
   },
